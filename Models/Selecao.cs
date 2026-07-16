8dc96703d8e1a8740df0d6ef26c53c223e3a67c7
miguel <matheusemiguelozzy@gmail.com>
2026-07-16 11:49:57 -0300

    Finalização do SelecaoController, retornando metodos http e deixando mais profissional e validação de dados na Model Selecao

--- Models/Selecao.cs
+++ Models/Selecao.cs
@@ -2,16 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; // Importa o namespace System.ComponentModel.DataAnnotations, que contém classes para validação de dados, como o atributo [Required] usado na propriedade Nome da classe Selecao.
 
 namespace DotNet_React_CopaDoMundo.Models
 {
     public class Selecao
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome da seleção é obrigatório.")]
+        [StringLength(
+        50,
+        MinimumLength = 3,
+        ErrorMessage = "O nome da seleção deve ter entre 3 e 50 caracteres."
+        )]
         public string Nome { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "O grupo da seleção é obrigatório.")]
+        [StringLength(
+            1,
+            MinimumLength = 1,
+            ErrorMessage = "O grupo da seleção deve ter exatamente 1 caractere."
+        )]
         public string Grupo { get; set; } = string.Empty;
-
+        
+        [Required(ErrorMessage = "A URL da bandeira da seleção é obrigatória.")]
+        [Url(ErrorMessage = "A URL da bandeira da seleção não é válida.")]
         public string BandeiraUrl { get; set; } = string.Empty;
 
         public List<Jogador> Jogadores { get; set; } = new List<Jogador>();
--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer(); // Adiciona suporte para explorar endpoints da API, permitindo a geração de documentação e testes interativos usando ferramentas como Swagger.
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -16,6 +19,8 @@ if (app.Environment.IsDevelopment())
 
 {
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapOpenApi();
 
 }
